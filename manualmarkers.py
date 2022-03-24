93f098b7fcf1c83e1d2ffec10f337762f97374a4
Lix3nn53 <lixakyildiz@gmail.com>
2022-03-24 09:23:17 +0300

    first commit

---
+++ manualmarkers.py
@@ -0,0 +1,2 @@
+mymarkers = [{'id':'town', 'x':2065, 'y':130, 'z':1311, 'name':'Guardian Academy', 'description':'The Guardian Academy is a place of study and research. It is a place where the students of the Academy can learn and grow.'},
+             {'id':'town', 'x':500, 'y':70, 'z': 100, 'name':'brownotopia', 'description':'Brownotopia is a town in the north-east of Adelia.'},]
